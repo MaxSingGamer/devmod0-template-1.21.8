4f00a7ef3a0f0abca213136bbd77c7f9e0ddf6a7
MaxSingGamer <maxsing@outlook.com>
2025-12-28 12:55:52 +0800

    Create - Upload

---
+++ src/client/java/com/maxshin/devmod0/Devmod0Client.java
@@ -0,0 +1,10 @@
+package com.maxshin.devmod0;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Devmod0Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/maxshin/devmod0/Devmod0DataGenerator.java
@@ -0,0 +1,11 @@
+package com.maxshin.devmod0;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Devmod0DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
